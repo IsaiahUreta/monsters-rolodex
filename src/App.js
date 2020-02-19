ecf4692f2424cc193db9391676a1c4fbb9c2e634
Ureta <isaiah_ureta@medilink.com.ph>
2020-02-19 14:12:50 +0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+
+class App extends Component{
+  constructor(){
+    super(); //
+    this.state = {
+      monsters :  [],
+      searchfield: ''
+    };
+  }
+
+  componentDidMount(){ // load when the app component get called
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(Response => Response.json()) //take response from url and convert to json file
+    .then(users => this.setState({monsters: users }))  // take the users that we got at it and put users array
+    .catch(error => console.log('FAILED'))
+  }
+
+  handleChange = e =>{
+    this.setState({searchfield: e.target.value})
+
+  }
+
+    
+
+  render(){
+    const {monsters, searchfield} =  this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchfield.toLowerCase())
+      );
+
+    return (
+      <div className="App">
+
+        <h1> Monster Rolodex</h1>
+       
+       <SearchBox 
+       placeholder='Search Monsters'
+       handleChange = {this.handleChange}
+       />
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    ); 
+  } 
+} 
 
 export default App;
