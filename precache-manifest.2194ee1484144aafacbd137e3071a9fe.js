cba1a2b3956cc69f89bb257c64574962a4e9e0a8
Ureta <isaiah_ureta@medilink.com.ph>
2020-02-19 14:09:34 +0800

    Updates

---
+++ precache-manifest.2194ee1484144aafacbd137e3071a9fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e51349a86f058413a34af6e07158541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09456dfaf7683dc8896d",
+    "url": "/static/css/main.95ae50d4.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1f3314c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09456dfaf7683dc8896d",
+    "url": "/static/js/main.413a12bc.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
